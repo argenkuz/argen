00b00d6a12b29385edf8b3698a8437e012f5afe2
unknown <argenkulzanov@gmail.com>
2023-12-04 20:47:18 +0600

    1

--- retot/retot.py
+++ retot/retot.py
@@ -1,19 +1,4 @@
 
-
-
-
-import random
-def int_validation(x):
-    x = input(f"{x}")
-    try:
-        x = int(x)
-        if x > 7 or x < 1:
-            print("invalid input")
-        else:
-            return x
-    except ValueError:
-        print("wrong input")
-        return 0
 killometres = [711,0,0,0,1,1,0]
 game = True
 while game:
